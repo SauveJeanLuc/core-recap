09935798d683fea382f8ca1c6c470e5435441720
SAUVE Jean-Luc <sauvejeanluc3@gmail.com>
2021-07-08 11:24:48 +0200

    clearScreen method for constant reload

--- src/TimeInSpecificFormat.java
+++ src/TimeInSpecificFormat.java
@@ -1,3 +1,4 @@
+import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.TimeZone;
@@ -7,7 +8,14 @@ import java.util.TimeZone;
  * @version 1.1
  */
 public class TimeInSpecificFormat {
-    public static void main(String[] args) {
+
+    //Used only in terminal
+    public static void clearScreen() {
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
+    }
+
+    public static void main(String[] args) throws IOException, InterruptedException {
         //Set format 2021-01-31 23:59:59.999
         SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss.SSS");
 
@@ -15,6 +23,9 @@ public class TimeInSpecificFormat {
         dateFormat.setCalendar(Calendar.getInstance(TimeZone.getTimeZone("GMT")));
 
         // Display the now time
-        System.out.println("Time(now): "+dateFormat.format(System.currentTimeMillis()));
+        while(true){
+            System.out.println("Time(now): "+dateFormat.format(System.currentTimeMillis()));
+        }
+
     }
 }
